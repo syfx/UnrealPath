45734ce1a10139413abf9e9957921683713bd9ee
suya <782933913@qq.com>
2019-05-08 12:16:26 +0800

    add a resource management container

---
+++ Assets/Resources/AssetManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "AssetManager")]
+public class AssetManager : ScriptableObject {
+
+    public static AssetManager GetAssetManager()
+    {
+        return Resources.Load<AssetManager>("MyAssetManager");
+    }
+
+    public List<Sprite> m_Background = new List<Sprite>();
+}
---
+++ Assets/Scripts/UIManagers/sptBg.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sptBg : MonoBehaviour {
+
+    private SpriteRenderer m_SpriteRenderer; 
+    private AssetManager m_AssetManager;            //资源管理器
+
+    public void Awake()
+    {
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
+        m_AssetManager =AssetManager.GetAssetManager();
+        //设置游戏背景
+        SetBackground();
+    }
+
+    /// <summary>
+    /// 设置游戏背景
+    /// </summary>
+    private void SetBackground()
+    {
+        //获取张随机游戏背景
+        int index = Random.Range(0, m_AssetManager.m_Background.Count);
+        m_SpriteRenderer.sprite = m_AssetManager.m_Background[index];
+    }
+}
